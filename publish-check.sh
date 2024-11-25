0f8ea4dab75e36d3416de10d3972b97c5a06217f
Jill Xu <jill@aptoslabs.com>
2024-11-25 15:09:31 -0800

    trial

--- publish-check.sh
+++ publish-check.sh
@@ -6,14 +6,14 @@ CHECK_ADDRESS="${CHECK_ADDRESS}"
 NAMED_ADDRESSES="${NAMED_ADDRESSES}"
 PRIVATE_KEY="${PRIVATE_KEY}"
 UPGRADE_ALLOWED="${UPGRADE_ALLOWED}"
-testnet_URL="https://fullnode.testnet.aptoslabs.com/"
-testnet_FAUCET_URL="https://faucet.testnet.aptoslabs.com/"
+TESTNET_URL="https://fullnode.testnet.aptoslabs.com/"
+TESTNET_FAUCET_URL="https://faucet.testnet.aptoslabs.com/"
 
 http_code=$(
   curl --write-out '%{http_code}' \
        --silent \
        --output /dev/null \
-       ${testnet_URL}v1/accounts/$CHECK_ADDRESS/resource/0x1::code::PackageRegistry
+       ${TESTNET_URL}v1/accounts/$CHECK_ADDRESS/resource/0x1::code::PackageRegistry
 )
 
 if [ $http_code -eq 404 ] || [ "$UPGRADE_ALLOWED" = "true" ]; then
@@ -23,12 +23,12 @@ if [ $http_code -eq 404 ] || [ "$UPGRADE_ALLOWED" = "true" ]; then
     --assume-yes"
   nix-shell $ACTION_PATH/shell.nix --command "aptos account fund-with-faucet \
     --account="$CHECK_ADDRESS" \
-    --url="$testnet_URL" \
-    --faucet-url="$testnet_FAUCET_URL""
+    --url="$TESTNET_URL" \
+    --faucet-url="$TESTNET_FAUCET_URL""
   nix-shell $ACTION_PATH/shell.nix --command "aptos move publish \
     --package-dir="$PACKAGE_DIR" \
     --named-addresses="$NAMED_ADDRESSES" \
-    --url="$testnet_URL" \
+    --url="$TESTNET_URL" \
     --assume-yes"
 elif [ $http_code -eq 200 ]; then
   echo "Package is already published at $CHECK_ADDRESS"
